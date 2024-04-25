4c209cfb95045bf9cf045a5413b2c82b6fbe2a46
kaushaladitya <95187334+kaushaladitya@users.noreply.github.com>
2024-04-25 14:05:35 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,21 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
-
-
-
-  
-
+    @staticmethod
+    def isValid(s: str) -> bool:
+        
+        leftSymbols = []
+      
+        for c in s:
+            
+            if c in ['(', '{', '[']:
+                leftSymbols.append(c)
+         
+            elif c == ')' and leftSymbols and leftSymbols[-1] == '(':
+                leftSymbols.pop()
+            elif c == '}' and leftSymbols and leftSymbols[-1] == '{':
+                leftSymbols.pop()
+            elif c == ']' and leftSymbols and leftSymbols[-1] == '[':
+                leftSymbols.pop()
+           
+            else:
+                return False
+        return not leftSymbols
